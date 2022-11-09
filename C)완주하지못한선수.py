08d961531ed0debff533ef1110b07fd21a41eee6
Ubuntu <ubuntu@ip-172-31-10-95.ap-northeast-2.compute.internal>
2022-11-09 08:45:42 +0000

    cmd에서 리드미 마지막줄 추가해줌 !

---
+++ C)완주하지못한선수.py
@@ -0,0 +1,19 @@
+a=["marina", "josipa", "nikola", "vinko", "filipa"]
+b=["josipa", "filipa", "marina", "nikola"]
+# c=[x for x in b if x not in b]
+# c= a-b
+# print(c)
+# c= set(c)
+# print(c)
+a.sort()
+b.sort()
+print(a)
+print(a[0])
+print(range(len(a)))
+print(b)
+for i in range(len(b)):
+    if a[i] != b[i]:
+        print(a[i])
+print(a[len(a)-1])
+
+
